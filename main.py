7f0c18b10fcba5322c2550ec432505297a94a15d
LaeLaeWin <emolay491997@gmail.com>
2018-08-24 09:55:16 -0700

    initial commit

---
+++ main.py
@@ -0,0 +1,35 @@
+
+from flask import Flask , render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/ztou')
+def ztou():
+    return render_template('ztou.html')
+
+@app.route('/ztow')
+def ztow():
+    return render_template('ztow.html')
+
+@app.route('/wtou')
+def wtou():
+    return render_template('wtou.html')
+
+@app.route('/wtoz')
+def wtoz():
+    return render_template('wtoz.html')
+
+@app.route('/utow')
+def utow():
+    return render_template('utow.html')
+
+@app.route('/utoz')
+def utoz():
+    return render_template('utoz.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
